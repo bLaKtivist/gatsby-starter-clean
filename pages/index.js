183f6ad2766f936994b4daa0e6594288b4665279
Krys Freeman <hello@krysfreeman.com>
2018-01-22 18:05:39 -0500

    try adding form from mit-iq
    see if that works outside of th ediv.

--- pages/index.js
+++ pages/index.js
@@ -20,6 +20,7 @@ export default class Index extends React.Component {
         </h1>
         <p>Welcome to your new clean Gatsby site</p>
       </div>
+      <form class="site-form" name="contact" autocomplete="off" method="post"><input type="hidden" name="form-name" value="contact"><fieldset class="fieldset"><input class="text-input" name="firstname" placeholder=" "><label class="field-label">First Name</label></fieldset><fieldset class="fieldset"><input class="text-input" name="lastname" placeholder=" "><label class="field-label">Last Name</label></fieldset><fieldset class="fieldset"><input class="text-input" type="email" name="email" required="" placeholder=" "><label class="field-label">Email</label></fieldset><fieldset class="fieldset"><textarea class="text-input input-textarea" row="6" name="inquiry" placeholder=" "></textarea><label class="field-label">Write inquiry here</label></fieldset><button class="form-submit button" type="submit">Submit</button></form>
     )
   }
 }
